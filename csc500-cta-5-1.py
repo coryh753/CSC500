27c47f9084293027e3d09afe86ef915338f55b47
Cory Hanson <chanson0521@gmail.com>
2024-01-01 09:38:27 -0500

    CTA 5 part 1 update variable name

--- csc500-cta-5-1.py
+++ csc500-cta-5-1.py
@@ -1,12 +1,12 @@
 try:
     # Ask for the number of years
-    num_years = int(input("Enter number of years: "))
+    numYears = int(input("Enter number of years: "))
 
-    total_rainfall = 0  # Total inches of rainfall for all months and years
-    num_months = num_years * 12  # Total number of months over the years
+    totalRainfall = 0  # Total inches of rainfall for all months and years
+    numMonths = numYears * 12  # Total number of months over the years
 
     # Outer loop iterates once for each year
-    for year in range(num_years):
+    for year in range(numYears):
         # Inner loop iterates twelve times for each month
         for month in range(12):
             # Ask the user for the inches of rainfall for that month
@@ -18,14 +18,14 @@ try:
                     print("Error: Rainfall cannot be less than zero")
 
             # Add the rainfall for the current month to the total
-            total_rainfall += rainfall
+            totalRainfall += rainfall
 
     # Calculate the average rainfall per month
-    avg_rainfall = total_rainfall / num_months
+    avgRainfall = totalRainfall / numMonths
 
     # Display the results
-    print(f"Total number of months: {num_months}")
-    print(f"Total inches of rainfall: {total_rainfall}")
-    print(f"Average rainfall per month: {avg_rainfall:.2f}")
+    print(f"Total number of months: {numMonths}")
+    print(f"Total inches of rainfall: {totalRainfall}")
+    print(f"Average rainfall per month: {avgRainfall:.2f}")
 except ValueError:
     print("Please enter a correct value")
